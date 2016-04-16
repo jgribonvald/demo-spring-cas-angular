bf62788acd57fd29f5cc20ee5a1ea4dc2710ca6d
Benevolent Dictator <wbsouza@gmail.com>
2016-04-16 15:29:14 -0700

    Solving the webapp issue with contextPath different of "/" https://github.com/jgribonvald/demo-spring-cas-angular/issues/2

--- src/main/java/org/esco/demo/ssc/security/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/org/esco/demo/ssc/security/AjaxAuthenticationSuccessHandler.java
@@ -9,17 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 /**
  * Spring Security success handler, specialized for Ajax requests.
  */
-//@Component
+@Component
 public class AjaxAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication)
-            throws IOException, ServletException {
+    public AjaxAuthenticationSuccessHandler() {
+        setDefaultTargetUrl("/");
+        setTargetUrlParameter("spring-security-redirect");
+    }
 
-        response.setStatus(HttpServletResponse.SC_OK);
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String targetUrl = this.determineTargetUrl(request, response);
+        if(response.isCommitted()) {
+            this.logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+        } else {
+            response.sendRedirect(targetUrl);
+        }
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        this.handle(request, response, authentication);
+        this.clearAuthenticationAttributes(request);
     }
 }
